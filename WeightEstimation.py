dedec5dce2af7d928fc3efe55db52edc9f7348b4
ahlamdavis <ahlam@ucdavis.edu>
2025-01-27 16:50:33 -0800

    Add files via upload

---
+++ WeightEstimation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+def RegressionData():
+
+    W0 = [3417,2866,6614,9259,4244,10000,7020,3500,3300,3900,6173,6100,10000,6000,6000,6000,12675]
+    We = [2229,1880,3550,5445,2242,4990,3525,2306,2229,2050,3660,2995,4500,3900,3550,3600,7120]
+
+    log_W0 = np.log10(W0)
+    log_We = np.log10(We)
+
+    B, A = np.polyfit(log_We, log_W0,1)
+
+error = 10^(-6)
+tol = 2*error
+W_guess = 5000
+W_crew = 180
+W_payload = 1
+
+def test():
+    print(A)
+def WeightEstimation():
+    
+    W0 = W_guess
+    while tol > error:
+        empty_weight_frac = 10^(-A/B) * W0^(1/B-1)
+
+
+        W0_new = (W_crew + W_payload)/(1-fuel_weight_ratio-empty_weight_frac)
+        error = (W0_new - W0)/W0_new
+        W0 = W0_new
+
+RegressionData()
+test()
