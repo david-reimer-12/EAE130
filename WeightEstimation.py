5fd5fe92d355529c5008b22fb237ddffa735f6b9
ahlamdavis <ahlam@ucdavis.edu>
2025-01-29 16:39:19 -0800

    Update WeightEstimation.py

--- WeightEstimation.py
+++ WeightEstimation.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-
-
+import math 
 ## Regression Data
 W0 = [3417,2866,6614,9259,4244,10000,7020,3500,3300,3900,6173,6100,10000,6000,6000,6000,12675]
 We = [2229,1880,3550,5445,2242,4990,3525,2306,2229,2050,3660,2995,4500,3900,3550,3600,7120]
@@ -10,17 +9,27 @@ log_We = np.log10(We)
 B, A = np.polyfit(log_We, log_W0,1)
 
 def WeightEstimation():
-    W_crew = 180
-    W_payload = 2000
+    W_crew = 180 #lbs
+    W_payload = 2000 #lbs
+    Range = 3220000 #ft
+    Endurance = 28800 #seconds
+    V_inf = 337.562 #ft/s
+    SFC = 0.25*32 #(lbm/hr)/lbf
+    prop_eff = 0.8
+    LD_ratio = 12
     W_guess = 5000
-    error = 10^(-6)
-    tol = 2*error
+    cruise = math.exp(-(Range*SFC)/(prop_eff*LD_ratio))
+    loiter = math.exp((-Endurance*V_inf*SFC)/(prop_eff*LD_ratio))
+    fuel_weight_ratio = 1 - 0.996*0.995*0.996*0.998*0.999*0.998*cruise*loiter
+    tol = 10**(-6)
+    error = 2*tol
     W0 = W_guess
-    while tol > error:
-        empty_weight_frac = 10^(-A/B) * W0^(1/B-1)
-
-        
+    while tol < error:
+        empty_weight_frac = 10**(-A/B) * W0**(1/B-1)
         W0_new = (W_crew + W_payload)/(1-fuel_weight_ratio-empty_weight_frac)
-        error = (W0_new - W0)/W0_new
+        error = abs((W0_new - W0)/W0_new)
         W0 = W0_new
+        print(W0)
 
+
+WeightEstimation()
